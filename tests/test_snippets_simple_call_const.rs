1887da4965d449126137909c2bfedb9c7176e732
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2024-12-01 13:14:50 +0100

    chore(test): add tests for `const`, `static` and `mut static`
    Signed-off-by: FedericoBruzzone <federico.bruzzone.i@gmail.com>

--- tests/test_snippets_simple_call_const.rs
+++ tests/test_snippets_simple_call_const.rs
@@ -1,6 +1,6 @@
 mod utils;
 
-mod test_snippets_simple_call_fn {
+mod test_snippets_simple_call_const {
     // use pretty_assertions::assert_eq;
     use crate::utils::run_with_cargo_bin_and_snippet;
 
@@ -16,15 +16,15 @@ mod test_snippets_simple_call_fn {
         Ok(())
     }
 
-    // #[test]
-    // fn test_simple_call_with_file_call_fn_alias() -> Result<(), String> {
-    //     let snippet = &std::fs::read_to_string(format!("{FOLDER}/call_fn_alias.rs")).unwrap();
-    //     let (output, _) = run_with_cargo_bin_and_snippet(snippet, &["--print-rl-graph"])?;
+    #[test]
+    fn test_simple_call_with_file_call_const_alias() -> Result<(), String> {
+        let snippet = &std::fs::read_to_string(format!("{FOLDER}/call_const_alias.rs")).unwrap();
+        let (output, _) = run_with_cargo_bin_and_snippet(snippet, &["--print-rl-graph"])?;
 
-    //     assert!(output.contains("0 -> 1")); // main -> test_own
+        assert!(output.contains("0 -> 1")); // main -> TEST 
 
-    //     Ok(())
-    // }
+        Ok(())
+    }
 
     // #[test]
     // fn test_simple_call_with_file_call_fn_ref_alias() -> Result<(), String> {
@@ -49,17 +49,17 @@ mod test_snippets_simple_call_fn {
         Ok(())
     }
 
-    // #[test]
-    // fn test_simple_call_with_file_call_fn_alias_from_closure() -> Result<(), String> {
-    //     let snippet =
-    //         &std::fs::read_to_string(format!("{FOLDER}/call_fn_alias_from_closure.rs")).unwrap();
-    //     let (output, _) = run_with_cargo_bin_and_snippet(snippet, &["--print-rl-graph"])?;
+    #[test]
+    fn test_simple_call_with_file_call_const_alias_from_closure() -> Result<(), String> {
+        let snippet =
+            &std::fs::read_to_string(format!("{FOLDER}/call_const_alias_from_closure.rs")).unwrap();
+        let (output, _) = run_with_cargo_bin_and_snippet(snippet, &["--print-rl-graph"])?;
 
-    //     assert!(output.contains("0 -> 1")); // main -> closure
-    //     assert!(output.contains("1 -> 2")); // closure -> test_own
+        assert!(output.contains("0 -> 1")); // main -> closure
+        assert!(output.contains("1 -> 2")); // closure -> test_own
 
-    //     Ok(())
-    // }
+        Ok(())
+    }
 
     // #[test]
     // fn test_simple_call_with_file_call_fn_ref_alias_from_closure() -> Result<(), String> {
